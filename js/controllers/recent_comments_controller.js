f1b764ca802aad8503b800670409723fc8dddab6
Steven Chang <prime_pork@hotmail.com>
2016-09-26 21:44:45 +1000

    added a function to RecentCommentsController that will return all comments in an array and now with a sortBy in the index.html, those comments are displayed by descending date

--- js/controllers/recent_comments_controller.js
+++ js/controllers/recent_comments_controller.js
@@ -2,5 +2,13 @@ BulletinBoard.controller("RecentCommentsController", ['$scope', '$http', '_', 'C
 	CommentsService.all()
 		.then(function(comments){ 
 			$scope.comments = comments;
+
+			$scope.commentsInArray = function(){
+				p = [];
+				_.each( comments, function(comment){ 
+					p.push( comment );
+				});
+				return p;
+			};
 		});
 }]);
