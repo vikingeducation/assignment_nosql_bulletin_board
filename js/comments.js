a333c43158ea015a27277f8e306b5e1f9a7bef87
David Jiang <davidjgrey@gmail>
2016-09-27 09:57:19 -0700

    comments show up

---
+++ js/comments.js
@@ -0,0 +1,50 @@
+"use strict";
+app.factory('CommentsSer', ['$http', '_', 'PostsSer', function($http, _, PostsSer){
+	
+	var _id;
+	var _comments;
+
+	var CommentsSer = {};
+
+	CommentsSer.all = function(){
+		return $http({
+			url: '/js/data/comments.json',
+			method: "GET"
+		})
+		.then(function(response){
+			return _comments = response.data;
+		});
+	};
+
+	CommentsSer.create = function(params){
+		var comment = angular.copy(params, {});
+		var nextId = CommentsSer.nextId();
+		comment.id = nextId;
+		comment.date = new Date();
+		comment.voteCount = 0;
+		_comments[nextId] = comment;
+		_id += 1;
+		PostsSer.addComment(comment.id);
+		return new Promise(function(resolve){
+			resolve(comment);
+		});
+	};
+
+	CommentsSer.nextId = function(){
+		if(!_id){
+			if(_.isEmpty(_comments)){
+				return _id = 1;
+			}
+			var ids = _.map(Object.keys(_comments), function(id){
+				return parseInt(id);
+			});
+			_id = _.max(ids);
+			return _id + 1;
+		}
+
+		return _id + 1;
+	};
+
+	return CommentsSer;
+
+}]);
--- js/posts_controller.js
+++ js/posts_controller.js
@@ -1,10 +1,21 @@
-app.controller('PostsCtrl', ['$scope', '_', 'PostsSer', function($scope, _, PostsSer){
+"use strict";
+
+app.controller('PostsCtrl', ['$scope', '_', 'PostsSer', 'CommentsSer', function($scope, _, PostsSer, CommentsSer){
   $scope.commentParams = {};
 
   PostsSer.all()
    .then(function(posts){
      $scope.posts = posts;
    });
+
+   CommentsSer.all()
+   .then(function(comments){
+    $scope.comments = comments;
+   });
+
+   $scope.createComment = function(){
+      CommentsSer.create($scope.commentParams);
+   };
 }]);
 
 
@@ -21,6 +32,10 @@ app.factory('PostsSer', ['$http', '_', function($http, _){
       .then(function(response){
         return _posts = response.data;
       });
+  };
+
+  PostsSer.addComment = function(id){
+    _posts["1"].comments.push(id);
   };
 
   return PostsSer;
