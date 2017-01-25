8a1d0d9575341ccd7f6e2d4265ecd63c366ed1a4
Sampson Crowley <sampsonsprojects@gmail.com>
2017-01-25 12:10:02 -0700

    additional features

--- js/services/comment.service.js
+++ js/services/comment.service.js
@@ -1,29 +1,51 @@
 bulletin.factory('commentService',[
-  '$http', '_', 'dateFormatService',
-  function($http, _, dateFormatService){
+  '$http', '$q', '_', 'dateFormatService',
+  function($http, $q, _, dateFormatService){
     var _comments = {};
-    var getComments = function getComments(){
-      return $http.get('/data/comments.json').then(function(resp){
-        angular.copy(resp.data, _comments);
-        dateFormatService.parse(_comments);
-        return _comments;
+    var _postComments = [];
+    var getComments = function getComments(refresh){
+      if(!_comments.length || refresh){
+        return $http.get('/data/comments.json').then(function(resp){
+          angular.copy(resp.data, _comments);
+          dateFormatService.parse(_comments);
+          return _comments;
+        });
+      }
+      return $q(function(resolve){
+        resolve(_comments)
       });
     };
 
-    var getCommentsByPost = function(list){
-      return getComments().then(function(){
-        var commentsArr = [];
-        for(var i = 0; i < list.length; i++){
-          commentsArr.push(_comments[list[i]]);
-        }
-        return commentsArr;
-      });
+    var refreshComments = function refreshComments(){
+      return getComments(true);
+    }
 
+    var getCommentsByIds = function(list){
+      return getComments().then(function(){
+        var _postComments.splice(0);
+        for(var i = 0; i < list.length; i++){
+          _postComments.push(_comments[list[i]]);
+        }
+        return _postComments;
+      });
+    };
+
+    var getCommentsByPost = function(postId){
+      return getComments().then(function(){
+        var _postComments.splice(0);
+        for(i in _comments){
+          if(_comments[i].post === postId){
+            _postComments.push(_comments[i]);
+          }
+        }
+        return _postComments;
+      });
     };
 
     return {
       all: getComments,
-      get: getCommentsByPost
+      get: getCommentsByIds,
+      refresh: refreshComments
     };
   }
 ]);
