ee61871d4103b06532d8edd0ec8a69901f516122
tt <ttwang.303@gmail.com>
2017-03-15 17:59:58 -0400

    display posts

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-var BulletinBoard = angular.module("noSQL", []);
+var BulletinBoard = angular.module("BulletinBoard", []);
+
+BulletinBoard.controller("PostsCtrl", ['$scope', "postService", function($scope, postService) {
+
+postService.getAll().then( function(posts) {
+  $scope.posts = posts;
+})
+
+}])
+
+BulletinBoard.factory("postService", ['$http', function($http) {
+  var postService = {};
+
+  var _posts;
+
+  postService.getAll = function() {
+    return $http({
+      method: "get",
+      url: "/data/posts.json"
+    }).then( function(response){
+      return _posts = response.data;
+    })
+  }
+
+  return postService;
+}])
